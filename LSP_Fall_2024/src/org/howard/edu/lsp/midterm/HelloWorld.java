0a5b4768fb5133f726f1414df5bdbb2d668294b1
Le'ale Addison <110490155+lealeaddison@users.noreply.github.com>
2024-10-14 18:24:33 -0400

    This is my initial commit for the updated pre-midterm exam instructions.

---
+++ LSP_Fall_2024/src/org/howard/edu/lsp/midterm/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * Le'ale Addison
+ */
+package org.howard.edu.lsp.midterm;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");
+	}
+}
